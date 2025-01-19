d71796161dc99e97b4a7c6093971bbb3fbacb9e6
samcool1224 <samaksh2405@gmail.com>
2025-01-19 00:20:26 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,15 +5,7 @@ import { getFirestore } from "firebase/firestore";
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyD2_vi8YVJowkYwTUVRL392MRj3CJCfoTE",
-  authDomain: "lendit-265ab.firebaseapp.com",
-  projectId: "lendit-265ab",
-  storageBucket: "lendit-265ab.appspot.com",
-  messagingSenderId: "931760947547",
-  appId: "1:931760947547:web:45ca9f1d8a8ce1360b1a7e",
-  measurementId: "G-70LWDLPWF2"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
